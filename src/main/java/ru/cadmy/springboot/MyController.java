b2cfb60e719920ec596c3112cec79c6a5aff9a13
cadmy <cadmy@ya.ru>
2016-03-06 02:17:07 +0300

    Include Spring-Security, set security blocks for index.html TODO Logining do not work

--- src/main/java/ru/cadmy/springboot/MyController.java
+++ src/main/java/ru/cadmy/springboot/MyController.java
@@ -1,7 +1,10 @@
 package ru.cadmy.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Cadmy on 05.03.2016.
@@ -12,5 +15,44 @@ public class MyController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "index";
+    }
+
+    @RequestMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    /** Error page. */
+    @RequestMapping("/error.html")
+    public String error(HttpServletRequest request, Model model) {
+        model.addAttribute("errorCode", "Error " + request.getAttribute("javax.servlet.error.status_code"));
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        StringBuilder errorMessage = new StringBuilder();
+        errorMessage.append("<ul>");
+        while (throwable != null) {
+            errorMessage.append("<li>").append(escapeTags(throwable.getMessage())).append("</li>");
+            throwable = throwable.getCause();
+        }
+        errorMessage.append("</ul>");
+        model.addAttribute("errorMessage", errorMessage.toString());
+        return "error.html";
+    }
+
+    /** Substitute 'less than' and 'greater than' symbols by its HTML entities. */
+    private String escapeTags(String text) {
+        if (text == null) {
+            return null;
+        }
+        return text.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
     }
 }
