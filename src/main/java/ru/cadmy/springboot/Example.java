36cf23b66136b5bfb89190ede33aa72661561462
cadmy <cadmy@ya.ru>
2016-03-05 23:45:54 +0300

    Set mapping, include thymeleaf

--- src/main/java/ru/cadmy/springboot/Example.java
+++ src/main/java/ru/cadmy/springboot/Example.java
@@ -1,15 +1,17 @@
+package ru.cadmy.springboot;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class Example {
 
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    public String meeting() {
+        return "meeting";
     }
 
     public static void main(String[] args) throws Exception {
