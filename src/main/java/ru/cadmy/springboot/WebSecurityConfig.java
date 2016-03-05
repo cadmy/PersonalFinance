23ac194ec689bcea668a7c59cd4129163ebc870a
cadmy <cadmy@ya.ru>
2016-03-06 02:32:15 +0300

    Resolve logining

--- src/main/java/ru/cadmy/springboot/WebSecurityConfig.java
+++ src/main/java/ru/cadmy/springboot/WebSecurityConfig.java
@@ -14,7 +14,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll();
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 
     @Autowired
