d883d71168ac49637a0459ad71331f2ae30f93cc
cadmy <cadmy@ya.ru>
2016-06-17 18:13:37 +0300

    Add tests for balance service

--- src/test/java/ru/cadmy/finance/service/BalanceServiceImplIT.java
+++ src/test/java/ru/cadmy/finance/service/BalanceServiceImplIT.java
@@ -1,6 +1,6 @@
 package ru.cadmy.finance.service;
 
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
@@ -11,7 +11,9 @@ import ru.cadmy.finance.model.*;
 
 import java.sql.Date;
 
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 
 /**
@@ -34,5 +36,44 @@ public class BalanceServiceImplIT {
         BalanceRecord balanceRecordTest = balanceService.getBalanceRecordById(balanceRecord.getId());
         assertEquals(balanceRecord, balanceRecordTest);
     }
+
+    @Test
+    @Rollback(true)
+    @Ignore
+    public void addBalanceRecordWithSameData() throws Exception {
+        User user = new User("IT@test.com", "test", "test", "test", Role.USER, State.ACTIVE, "IT");
+        BalanceRecord balanceRecord1 = new BalanceRecord(user, Date.valueOf("2016-06-02"), Sign.INCOME, 1000, "Test", "Test");
+        BalanceRecord balanceRecord2 = new BalanceRecord(user, Date.valueOf("2016-06-02"), Sign.INCOME, 1000, "Test", "Test");
+        balanceService.addBalanceRecord(balanceRecord1);
+        balanceService.addBalanceRecord(balanceRecord2);
+        BalanceRecord balanceRecordTest1 = balanceService.getBalanceRecordById(balanceRecord1.getId());
+        BalanceRecord balanceRecordTest2 = balanceService.getBalanceRecordById(balanceRecord2.getId());
+        assertThat(balanceRecordTest1, not(balanceRecordTest2));
+    }
+
+    @Test
+    @Rollback(true)
+    public void removeBalanceRecord() throws Exception {
+        User user = new User("IT@test.com", "test", "test", "test", Role.USER, State.ACTIVE, "IT");
+        BalanceRecord balanceRecord = new BalanceRecord(user, Date.valueOf("2016-06-02"), Sign.INCOME, 1000, "Test", "Test");
+        balanceService.addBalanceRecord(balanceRecord);
+        long balanceRecordId = balanceRecord.getId();
+        balanceService.removeBalanceRecord(balanceRecordId);
+        BalanceRecord balanceRecordTest = balanceService.getBalanceRecordById(balanceRecordId);
+        assertEquals(balanceRecordTest, null);
+    }
+
+    @Test
+    @Rollback(true)
+    public void getBalanceRecordList() throws Exception {
+        User user = new User("IT@test.com", "test", "test", "test", Role.USER, State.ACTIVE, "IT");
+        BalanceRecord balanceRecord = new BalanceRecord(user, Date.valueOf("2016-06-02"), Sign.INCOME, 1000, "Test", "Test");
+        balanceService.addBalanceRecord(balanceRecord);
+        long id = balanceRecord.getId();
+        balanceService.removeBalanceRecord(id);
+        BalanceRecord balanceRecordTest = balanceService.getBalanceRecordById(balanceRecord.getId());
+        assertEquals(balanceRecord, balanceRecordTest);
+    }
+
 
 }
