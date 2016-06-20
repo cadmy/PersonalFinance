96120ed4bf0b3e5c1bf247402b97407781aa0957
cadmy <cadmy@ya.ru>
2016-06-20 17:08:07 +0300

    Download local css and js libs

--- src/main/java/ru/cadmy/finance/configuration/WebSecurityConfig.java
+++ src/main/java/ru/cadmy/finance/configuration/WebSecurityConfig.java
@@ -47,6 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .antMatchers("/images/**")
-                .antMatchers("/js/**");
+                .antMatchers("/js/**")
+                .antMatchers("/css/**");
     }
 }
